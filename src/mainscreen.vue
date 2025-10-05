9692cddc986d6d911b77fdba2792b1fc6d6a44af
Visut Hsu <visuthsu@gmail.com>
2025-10-06 00:39:30 +0700

    changed title

--- src/mainscreen.vue
+++ src/mainscreen.vue
@@ -9,7 +9,7 @@ const state = useGameState()
 <template>
 
   <div class="w-screen h-screen flex flex-col items-center justify-evenly py-[20%]">
-    <p class="text-4xl"> game name here </p>
+    <p class="text-4xl"> Text <span class="underline">collector</span> </p>
     <button
       class="text-2xl bg-gray-400 hover:bg-gray-500 transition-colors duration-200 p-2 rounded-full w-2/5 flex justify-center"
       @click='state.scene = Tutorial'>
