2f812e490a1c14da113f8ab8f35237a59e2d6c42
Visut Hsu <visuthsu@gmail.com>
2025-10-07 00:46:01 +0700

    updated dep

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwind from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
     tailwind()
   ],
+  base: "/html/15165563",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
